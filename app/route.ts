6fd906ca62ed683caea21cd9f7f68dd4f755111b
Ddrewy <deandrew.y@gmail.com>
2026-03-09 12:55:29 +1100

    update: updating the api so it is going to run for 2026 course outline

--- app/route.ts
+++ app/route.ts
@@ -1,22 +1,37 @@
 import { NextResponse } from "next/server";
 
-interface Resp {
-  name: String,
-  weight: String
+interface Assessment {
+  name: string,
+  weight: number
 }
 
 export async function GET(request: Request) {
-  let respArr: Resp[] = [];
-  const req = await request.json();
-  const {course, term} = req; 
-  const year:number = new Date().getFullYear();
-  const resp = await fetch(`https://courseoutlines.unsw.edu.au/v1/publicsitecourseoutlines/detail?year=${year}&term=Term+${term}&deliveryMode=Multimodal&deliveryFormat=Standard&teachingPeriod=T${term}&deliveryLocation=Kensington&courseCode=${course}&activityGroupId=1`);
-  const res:JSON = JSON.parse(resp); 
+  const searchParams = new URLSearchParams(new URL(request.url).searchParams);
+  const course = searchParams.get('course');
+  const term = searchParams.get('term'); 
 
-  for (let i : res.integrat_CO_Assesment) {
-    respArr[i].name = res.integrat_CO_Assesment.i.integrat_title;
-    respArr[i].weight = res.integrat_CO_Assesment.i.integrat_weight;
+  //check the data if the params is available
+  if (!course || !term) {
+    return NextResponse.json({error: "Missing Parameter"}, {status: 401});
+  }
+  const year:number = new Date().getFullYear();
+  console.log(year);
+  const fetchResponse = await fetch(`https://courseoutlines.unsw.edu.au/v1/publicsitecourseoutlines/detail?year=${year}&term=Term+${term}&deliveryMode=In+Person&deliveryFormat=Standard&teachingPeriod=T${term}&deliveryLocation=Kensington&courseCode=${course}&activityGroupId=1`);
+
+  // check if the fetch is good or not
+  if (!fetchResponse.ok) {
+    return NextResponse.json({error: "failed to fetch data"}, {status: 500});
   }
 
-  return new Response();
+  const data = await fetchResponse.json(); 
+  console.log(data);
+  const rawAssessment = data.integrat_CO_Assessment;
+
+  const respArr: Assessment[] = rawAssessment.map((item: any) => ({
+    name: item.integrat_title,
+    weight: Math.round(parseFloat(item.integrat_weight.replace("%", "")) * 100),
+  }));
+
+  // return the data that have been compiled
+  return NextResponse.json(respArr, {status:200});
 }
